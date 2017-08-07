30698d90a9a18050153e82f03ac89da613b5d7c7
ClausOlsen72 <30793350+ClausOlsen72@users.noreply.github.com>
2017-08-07 12:26:29 +0200

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using CamillaApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CamillaApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MapControl_MapTapped(Windows.UI.Xaml.Controls.Maps.MapControl sender, Windows.UI.Xaml.Controls.Maps.MapInputEventArgs args)
+        {
+            WeatherViewModel vm = (WeatherViewModel) this.DataContext;
+            vm.Latitude = args.Location.Position.Latitude;
+            vm.Longitude = args.Location.Position.Longitude;
+            vm.WeatherCommand.Execute(null);
+        }
+    }
+}
---
+++ ViewModel/WeatherViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamillaApp.ViewModel
+{
+   public class WeatherViewModel : ViewModelBase
+    {
+        double latitude;
+        double longitude;
+
+        public RelayCommand WeatherCommand { get; private set; }
+
+        public double Latitude
+        {
+            get
+            {
+                return latitude;
+            }
+
+            set
+            {
+                latitude = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Longitude
+        {
+            get
+            {
+                return longitude;
+            }
+
+            set
+            {
+                longitude = value;
+                OnPropertyChanged();
+            }
+        }
+
+        bool execute = true;
+
+        public WeatherViewModel()
+        {
+            WeatherCommand = new RelayCommand(GetWeather,() => execute);
+        }
+
+        private void GetWeather()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
